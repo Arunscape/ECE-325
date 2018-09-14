b69a1acef85ebab42150542af016db825c2043e5
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-09-13 22:21:33 -0600

    wish we were using java 8

--- Assignments/Assignment1/daysold/src/daysold.java
+++ Assignments/Assignment1/daysold/src/daysold.java
@@ -8,6 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.text.DateFormat;
+import java.time.Instant;
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class daysold {
 
@@ -17,19 +19,32 @@ public class daysold {
      */
     public static void days(String birthday) {
   
-//        	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-dd-MM");
+        	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//    	SimpleDateFormat formatter = new SimpleDateFormat("yyyy-dd-MM");
     	
     	Calendar today = GregorianCalendar.getInstance();
+    		today.set(Calendar.HOUR_OF_DAY, 0);
+    		today.set(Calendar.MINUTE, 0);
+    		today.set(Calendar.SECOND, 0);
+    		today.set(Calendar.MILLISECOND, 0);
     	Calendar bday = GregorianCalendar.getInstance();
+    		bday.set(Calendar.HOUR_OF_DAY, 0);
+    		bday.set(Calendar.MINUTE, 0);
+    		bday.set(Calendar.SECOND, 0);
+    		bday.set(Calendar.MILLISECOND, 0);
+    		
 ;        	try { // Attempt to parse user input
         		Date date = formatter.parse(birthday);
         		bday.setTime(date) ;
-        		System.out.println();
+       		
+        		//delet this
+//        		Date bleh = formatter.parse("2000-06-15");
+//            	today.setTime(bleh);
         	}
  			catch (java.text.ParseException e) {
 //				e.printStackTrace();
- 				System.out.println("Invalid birthday! Must be in format: yyyy-dd-MM");
+ 				System.out.println("Invalid birthday! Must be in format: yyyy-MM-dd");
+// 				System.out.println("Invalid birthday! Must be in format: yyyy-dd-MM");
 			} 	
 	 	
     	// format output strings 
@@ -37,24 +52,45 @@ public class daysold {
     	String formattedbDay = outputFormat.format(bday.getTime());
     	String formattedToday = outputFormat.format(today.getTime());
     	
-    	// find age in milliseconds, then convert to days if the date is valid
-    	long ageMillisecs =  today.getTimeInMillis()-bday.getTimeInMillis();
+//    	find age in milliseconds, then convert to days if the date is valid
+//    	there are  86400000 milliseconds in a day ( Math.round will take care of daylight savings's 1 hour difference 
+//     BECAUSE WE ARE NOT USING JAVA8 AND INSTANCES AREN'T IN THE JAVA7 STANDARD LIBRARY
     	
-    	if (ageMillisecs < 0  ) {
-    		System.out.println(String.format("Invalid birthday: %s, you cannot be born before today: %s", formattedbDay, formattedToday));
-    		return;
-    	}
-    	
-    	int daysOld =  (int) (ageMillisecs/(24 * 60 * 60 * 1000));
-        	
-    	System.out.println(String.format(    	
-    	    	"Birthday: %s; today: %s -- You are %d days old.", formattedbDay, formattedToday, daysOld));
-    	
-    	
-    
+    	long daysOld = Math.round((today.getTime().getTime() - bday.getTime().getTime()) / (double) 86400000);
+    	if(daysOld<0) {
+		System.out.println(String.format("Birthday: %s; today: %s -- Wrong birthday!", formattedbDay, formattedToday));
+		return;
+		}
     	
 
-    } // public static void days(String birthday)
+//	UGH WHY CAN'T WE USE JAVA 8 THIS WOULD BE SO MUCH BETTER
+//    	░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+//    	░░░░░▄█▀▀░░░░▄▄▄▄░░░░▄░░░░░░░░░░░░░░░░░
+//    	▄░░░░▀░░░░▄█▀▀░░░░░░░█░░░░░░░░░░░░▄▄▄░▀
+//    	░▀▄▄░░░░░▀▀░░░▄▄▄▄░░█░░░░░░░░░░▄▄█░░░░░
+//    	░░░░▀▀▄░░░░░░▀▀░░░░░▀▄░░░░░░▄▄█████▀█▄░
+//    	░░░░░░░▀▀▄▄░░░░░░░░░░█▄░░░░██▀▀▀▄█▀▀▀▀▄
+//    	░░░░░░░░░░░▀▀▄░░░░░░░░▀▄░░▄██░░░░░░░░░█
+//    	░░░░░░░░░░░░░░▀▀▄░░░░░░░▀▄▄██░░░░░░░░█▀
+//    	░░░░░░░░░░░░░░░░▄██▄░░░░░░█▀▀█▀░▄▄▀▀▀░░
+//    	░░░░░░░░░░░░░░▄█▀░▀▀░░░░░█░░█░▀▀▀█░░░░░
+//    	░░░░░░░░░░░░░██░░░░░░░░░░█░░█░░░░█░░░░░
+//    	▄░░░░░░░░░░▄█▀░░░░░░░░░▄░█░▄▀░░░█░░░░░░
+//    	░▀▄░░░░░░▄█▀░░░░░▄▀████████▀▀▀▀█▀░░░░░░
+//    	░░░▀▀▄▄▄█▀░░░░░░░█░▀███▀██▀▀▀▀██░░░░░░░
+//    	░░░░░░░▀██░░░░░░█▀░░▀██░░█░░░░██░░░░░░░
+//    	░░░░░░░░░░░░░░░░█░░░░███░█░░░░█▀░░░░░░░
+//    	░░░░░░░░░░░░░░░░▀▄░░░░▀▀▄░░░░░█░░░░░░░░
+//    	░░░░░░░░░░░░░░░░░░░░░░░░▀▄░░░░░░░░░░░░░
+//    	int daysOld =  (int) DAYS.between(bday.getTime().toInstant(),today.getTime().toInstant()); //getTime returns a Date object, which gets converted to an instant so that java can calculate the days between
+//    	if(daysOld<0) {
+//    		System.out.println(String.format("Birthday: %s; today: %s -- Wrong birthday!", formattedbDay, formattedToday));
+//    		return;
+//    		}
+    	
+    	System.out.println(String.format(    	
+    	    	"Birthday: %s; today: %s -- You are %d days old.", formattedbDay, formattedToday, daysOld));
+    } 
 
     /**
      * Main entry
@@ -62,7 +98,16 @@ public class daysold {
      */
     public static void main(String[] args) {
         days("2000-01-01");
-        days("3000-01-01");           // This is a wrong birthday
-    } // public static void main(String[] args)
+        days("3000-01-01");  // This is a wrong birthday
+        days("1920-01-01"); 
+        days("1700-03-26"); // suuuper long time ago
+        days("1920-02-29"); // leap day I think
+        days("2018-12-01"); // future
+        days("2018-09-13"); // today
+        days("2005-04-28"); 
+
+        
+    } 
 
 }
+
