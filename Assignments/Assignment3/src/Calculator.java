6359e85bc3dab2dcdcb7f06efea67fd7f30c2eb2
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-10-03 23:29:22 -0600

    fuck

--- Assignments/Assignment3/src/Calculator.java
+++ Assignments/Assignment3/src/Calculator.java
@@ -16,11 +16,12 @@ private Pattern findExponents = Pattern.compile("[0-9]+ \\^ [0-9]+");
 private Pattern findMulDiv = Pattern.compile("[0-9]+ (\\*|/) [0-9]+");
 private Pattern findAddSub = Pattern.compile("[0-9]+ (\\+|-) [0-9]+");
 private Pattern findAssignments = Pattern.compile("let [a-z] = ([a-z]|[0-9]+)");
+private int answer;
 
-private void findOperation(String input, HashMap<String,Integer> vars){
+private int findOperation(String input, HashMap<Character,Integer> vars){
 		Matcher m = null;
 		
-		HashMap<String,Integer> dict;
+		HashMap<Character,Integer> dict;
 		if (vars==null) {
 			dict=new HashMap<>();
 		}
@@ -34,17 +35,22 @@ private void findOperation(String input, HashMap<String,Integer> vars){
         else if (findMulDiv.matcher(input).find())           { m=findMulDiv.matcher(input);           }
         else if (findAddSub.matcher(input).find())          { m=findAddSub.matcher(input);         }
         else if (findAssignments.matcher(input).find()) { m=findAssignments.matcher(input); }
-        else {return;}
+        else {return answer;}
         // R E C U R S I O N
 //        System.out.println(input);
         
         
        if(m.find()) {
 //    	   System.out.println(input.substring(m.start(), m.end()));
+//    	   System.out.print(m.start());
+//    	   System.out.print(" ");
+//    	   System.out.println(m.end());
+//    	   System.out.println(input.indexOf(")"));
+//    	   System.out.println(input);
          calculate(input, m.start(), m.end(),dict);
         }
-        
-
+       return answer;
+       
 }
 
 //private int calculate(int a, String op, int b) {
@@ -55,13 +61,31 @@ private void findOperation(String input, HashMap<String,Integer> vars){
 //	return -1;
 //}
 
-private void calculate(String input, int a, int b, HashMap<String,Integer> vars) {
-	int sol=-1;
-	Matcher m = findAssignments.matcher(input);
+private void calculate(String input, int a, int b, HashMap<Character,Integer> vars) {
+	int sol;
+	String exp = input.substring(a,b);
+	Matcher m = findAssignments.matcher(exp);
 	if (m.find()) {
-		// find the variable
-		System.out.println(m.group());
+		// in an expression let x = #, the variable is; always the 4th index in the string
+		// the value is the substring from index 8 onwards
+//		System.out.println(m.group());
+//		System.out.println(m.group().charAt(4));
+//		System.out.println(m.group().substring(8));
+		sol= Integer.parseInt(m.group().substring(8));
+		vars.put(m.group().charAt(4), sol);
+		
 	}
+	else {
+		String[] stuff = exp.split(" ");
+		System.out.println(stuff);
+//		Matcher m1 = Pattern.compile("[*/+-]").matcher(input);
+//		if(m1.find()) {
+//			String op = m1.group();
+//			System.out.println(op);
+//		}
+		sol = 10;
+	}
+	answer=sol;
 	
 //	System.out.println(input);
 //	String op = Pattern.compile("[*/+-]").matcher(input).group();
@@ -70,7 +94,7 @@ private void calculate(String input, int a, int b, HashMap<String,Integer> vars)
 	findOperation(
 			input.substring(0, a)+
 			Integer.toString(sol)+
-			input.substring(b, input.length()-1)
+			input.substring(b)
 			,vars
 			);
 	
@@ -83,9 +107,9 @@ public int execExpression(String exp) {
 
         // TODO: Assignment 3 Part 2-1 -- when come to illegal expressions, raise proper exceptions
         
-        findOperation(exp,null);
+        return findOperation(exp,null);
         
-        return returnValue;
+//        return returnValue;
 }
 
 /**
@@ -110,7 +134,7 @@ public static void main(String[] args) {
 //                System.out.println(String.format("%d -- %-90s %d", i+1, inputs[i], calc.execExpression(inputs[i])));
 //        for (int i = 0; i < inputs.length; i++)
 //        	calc.execExpression(inputs[i]);
-        calc.execExpression(inputs[1]);
+        System.out.println(calc.execExpression(inputs[1]));
 
         // Part 2
         inputs = new String[] {
