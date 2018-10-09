8731e8afe0f6adb431ed362d63eb08d323d924ea
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-10-09 17:47:54 -0600

    turns out Double and double are different...

--- Assignments/Assignment4/UnitTesting/Source/Calculator.java
+++ Assignments/Assignment4/UnitTesting/Source/Calculator.java
@@ -6,68 +6,18 @@
 
 class Calculator{
 	
-//	public Number add(Number x, Number y) {
-//		System.out.println(x.getClass().getName());
-//		if (x instanceof Integer || y instanceof Integer) {
-//			System.out.println(x.intValue()+y.intValue());
-//			return x.intValue()+y.intValue();
-//		}
-//		else if (x instanceof Double || y instanceof Double) {
-//			System.out.println(x.doubleValue()+y.doubleValue());
-//			return x.doubleValue()+y.doubleValue();
-//		}
-//		else {
-//			return Float.NaN;
-//		}
-//	}
-	
-	public static Double add(Double x, Double y) {
+	public static Double add(double x, double y) {
 		return x+y;
 	}
-	public static int add(int x, int y) {
-		return x+y;
-	}
-	
-	public static Double subtract( Double x, Double y) {
+	public static Double subtract(double x, double y) {
 		return x-y;
 	}
-		
-	
-	public Number multiply(Number x, Number y) {
-		System.out.println(x.getClass().getName());
-		if (x instanceof Integer || y instanceof Integer) {
-			System.out.println(x.intValue()+y.intValue());
-			return x.intValue()*y.intValue();
-		}
-		else if (x instanceof Double || y instanceof Double) {
-			System.out.println(x.doubleValue()+y.doubleValue());
-			return x.doubleValue()*y.doubleValue();
-		}
-		else {
-			return Float.NaN;
-		}
+	public static Double multiply(double x, double y) {
+		return x*y;
 	}
-
-	public Number divide(Number x, Number y) {
-		System.out.println(x.getClass().getName());
-		if (x instanceof Integer || y instanceof Integer) {
-			System.out.println(x.intValue()+y.intValue());
-			return x.intValue()/y.intValue();
-		}
-		else if (x instanceof Double || y instanceof Double) {
-			System.out.println(x.doubleValue()+y.doubleValue());
-			return x.doubleValue()/y.doubleValue();
-		}
-		else {
-			return Float.NaN;
-		}
+	public static Double divide(double x, double y) {
+		return x/y;
 	}
-	
-//	
-//	public static void main(String[] args) {
-//		System.out.println(add(1,-1));
-//		;
-//	}
 }
 
 
