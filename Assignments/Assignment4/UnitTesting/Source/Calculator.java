269d92e7699e80e41f65a544f2741e456c62be6d
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-10-09 17:44:13 -0600

    lmao this is so wrong

--- Assignments/Assignment4/UnitTesting/Source/Calculator.java
+++ Assignments/Assignment4/UnitTesting/Source/Calculator.java
@@ -4,3 +4,70 @@
  */
 // TODO: Assignment 4 Part 2 -- Create the Calculator here
 
+class Calculator{
+	
+//	public Number add(Number x, Number y) {
+//		System.out.println(x.getClass().getName());
+//		if (x instanceof Integer || y instanceof Integer) {
+//			System.out.println(x.intValue()+y.intValue());
+//			return x.intValue()+y.intValue();
+//		}
+//		else if (x instanceof Double || y instanceof Double) {
+//			System.out.println(x.doubleValue()+y.doubleValue());
+//			return x.doubleValue()+y.doubleValue();
+//		}
+//		else {
+//			return Float.NaN;
+//		}
+//	}
+	
+	public static Double add(Double x, Double y) {
+		return x+y;
+	}
+	public static int add(int x, int y) {
+		return x+y;
+	}
+	
+	public static Double subtract( Double x, Double y) {
+		return x-y;
+	}
+		
+	
+	public Number multiply(Number x, Number y) {
+		System.out.println(x.getClass().getName());
+		if (x instanceof Integer || y instanceof Integer) {
+			System.out.println(x.intValue()+y.intValue());
+			return x.intValue()*y.intValue();
+		}
+		else if (x instanceof Double || y instanceof Double) {
+			System.out.println(x.doubleValue()+y.doubleValue());
+			return x.doubleValue()*y.doubleValue();
+		}
+		else {
+			return Float.NaN;
+		}
+	}
+
+	public Number divide(Number x, Number y) {
+		System.out.println(x.getClass().getName());
+		if (x instanceof Integer || y instanceof Integer) {
+			System.out.println(x.intValue()+y.intValue());
+			return x.intValue()/y.intValue();
+		}
+		else if (x instanceof Double || y instanceof Double) {
+			System.out.println(x.doubleValue()+y.doubleValue());
+			return x.doubleValue()/y.doubleValue();
+		}
+		else {
+			return Float.NaN;
+		}
+	}
+	
+//	
+//	public static void main(String[] args) {
+//		System.out.println(add(1,-1));
+//		;
+//	}
+}
+
+
