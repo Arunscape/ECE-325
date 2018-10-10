207da70a1fe45105d2f8342a95014be72d2eef85
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-10-09 19:28:48 -0600

    q1,2,3 done

--- Assignments/Assignment4/UnitTesting/Source/Calculator.java
+++ Assignments/Assignment4/UnitTesting/Source/Calculator.java
@@ -9,15 +9,31 @@ class Calculator{
 	public static Double add(double x, double y) {
 		return x+y;
 	}
+	
 	public static Double subtract(double x, double y) {
 		return x-y;
 	}
+	
 	public static Double multiply(double x, double y) {
 		return x*y;
 	}
+	
 	public static Double divide(double x, double y) {
 		return x/y;
 	}
+	
+	public static Double[] getRoots(double a, double b, double c) {
+		// ax²+bx+c
+		// x=(-b +/- √b²-4ac)/(2a)
+		Double x1 = (-b+Math.sqrt(b*b-4*a*c))/(2*a);
+		Double x2 = (-b-Math.sqrt(b*b-4*a*c))/(2*a);
+		System.out.print(x1);
+		System.out.print(" ");
+		System.out.println(x2);
+		
+		return x1.equals(x2) && (!x1.isNaN() && !x2.isNaN()) ? new Double[] {x1} : new Double[]{x1,x2};
+	}
+	
 }
 
 
