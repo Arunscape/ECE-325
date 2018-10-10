936623bd7f228e096c465d579722115fd2db1e20
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-10-09 19:51:22 -0600

    lol is it that easy?

--- Assignments/Assignment4/UnitTesting/Test Cases/SquareRootTests.java
+++ Assignments/Assignment4/UnitTesting/Test Cases/SquareRootTests.java
@@ -9,8 +9,10 @@ import org.junit.Test;
 public class SquareRootTests {
 
     // TODO: Assignment 4 Part 2 -- Checkpoint4
+	private Calculator calc;
 
     @Before public void setUp() throws Exception {
+    	calc = new Calculator();
     }
 
     @After public void tearDown() throws Exception {
@@ -18,22 +20,33 @@ public class SquareRootTests {
 
     @Test public void testRandomPositiveSquareRoot() {
         // You cannot use the Math.sqrt() function in the test!
+    	double ans = (Math.random() - 0.5) * 200000000;
+    	while (ans <= 0) {
+    		ans = (Math.random() - 0.5) * 200000000;
+    	}
+    	double x = ans*ans;
+    	assertEquals(calc.sqrt(x),Double.valueOf(ans));   	
         
     }
 
     @Test public void testRandomNegitiveSquareRoot() {
         // The result should be a complex number i.e. Double.isNaN()
+    	double x = (Math.random() - 0.5) * 200000000;
+    	while (x > 0) {
+    		x = (Math.random() - 0.5) * 200000000;
+    	}
+    	assertTrue(calc.sqrt(x).isNaN());
         
     }
 
     @Test public void testSquareRootofZero() {
         // You cannot use the Math.sqrt() function in the test!
-        
+        assertEquals(calc.sqrt(0),Double.valueOf(0));
     }
 
     @Test public void testSquareRootofOne() {
         // You cannot use the Math.sqrt() function in the test!
-        
+    	assertEquals(calc.sqrt(1),Double.valueOf(1));
     }
 
 }
